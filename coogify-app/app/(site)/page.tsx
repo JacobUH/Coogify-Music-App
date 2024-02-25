fe11dc77a1fcb3579903eb51347a0d7dee57e44c
Jacob Rangel <jacobrangel0628@gmail.com>
2024-02-24 18:43:34 -0600

    configuring enviroment

---
+++ coogify-app/app/(site)/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="text-purple-500">Welcome to Coogify Music</div>;
+}
---
+++ coogify-app/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Coogify Music",
+  description: "Upcoming online music library!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
