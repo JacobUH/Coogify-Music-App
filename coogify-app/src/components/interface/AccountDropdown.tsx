aa658d4c452e4fd4463ef05d0eed781306e219e8
Shruthi Yenamagandla <yshruthi.14@gmail.com>
2024-03-17 18:54:20 -0500

    Emma and Shruthi worked on the dropdown menu

--- coogify-app/src/components/interface/AccountDropdown.tsx
+++ coogify-app/src/components/interface/AccountDropdown.tsx
@@ -5,7 +5,17 @@ export const AccountDropdown = () => {
     <div>
       <div className="">
         <img src={ProfileIcon} alt="Profile Icon" className=""></img>
-        <div className="">{/* Work Here */}</div>
+      
+        <div className="pt-32 pr-32">
+          <div className="bg-gray-700 text-white rounded-lg text-right">
+            {/* Work Here */}
+            <a href='#' className="block m-2 px-3 py-3 hover:bg-gray-600">Profile</a>
+            <a href='#' className="block m-2 px-3 py-3 hover:bg-gray-600">Subscription</a>
+            <a href='#' className="block m-2 px-3 py-3 hover:bg-gray-600 border-b-2">Payment</a>
+            <a href='#' className="block m-2 px-3 py-3 hover:bg-gray-600">Logout</a>
+          </div>
+        </div>
+
       </div>
     </div>
   );
