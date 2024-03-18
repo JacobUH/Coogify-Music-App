2fd0627c1cf00aa8ad7c463c2034612ccb3a4fe9
Jacob Rangel <jacobrangel0628@gmail.com>
2024-03-18 17:49:11 -0500

    worked on navbar -jacob

--- coogify-app/src/components/interface/Navbar.tsx
+++ coogify-app/src/components/interface/Navbar.tsx
@@ -3,10 +3,10 @@ import { AccountDropdown } from './AccountDropdown';
 
 export const Navbar = () => {
   return (
-    <div className=" w-full h-12 px-5 py-14 relative top-0 pl-[400px] md:flex items-center justify-between cursor-pointer">
+    <div className=" w-full h-12 px-5 py-14 relative top-0 pl-[400px] md:flex items-center justify-between">
       {/* Logo on the top left */}
       <div>
-        <img src={LogoIcon} alt="Logo" className="h-14" />
+        <img src={LogoIcon} alt="Logo" className="h-14 cursor-pointer" />
       </div>
 
       {/* Profile icon on the top right */}
