deb221fdc13deb76f3c5cc441a75a8197b002ad4
lindolfo1 <cuentas@feptec.com>
2024-04-05 17:23:02 -0500

    modified:   coogify-app/backend/api/artist/artistSetup.js 	modified:   coogify-app/backend/api/fetch/song.js 	modified:   coogify-app/backend/api/home/fetchNewSongs.js 	modified:   coogify-app/backend/api/home/fetchRBSongs.js 	modified:   coogify-app/backend/api/home/fetchRapSongs.js 	modified:   coogify-app/backend/api/login.js 	modified:   coogify-app/backend/api/register.js 	modified:   coogify-app/backend/middlewares/authenticate.js 	modified:   coogify-app/backend/middlewares/hashPassword.js 	modified:   coogify-app/backend/middlewares/jsonParser.js

--- coogify-app/backend/api/artist/artistSetup.js
+++ coogify-app/backend/api/artist/artistSetup.js
@@ -1,29 +1,31 @@
-import { insertArtist } from "../../database/queries/dbArtistQueries.js";
-import { extractSessionId, errorMessage } from "../../util/utilFunctions.js";
-import jsonParserMiddleware from "../../middlewares/jsonParser.js";
-import hashPasswordMiddleware from "../../middlewares/hashPassword.js";
-import authenticateMiddleware from "../../middlewares/authenticate.js";
+import { insertArtist } from '../../database/queries/dbArtistQueries.js';
+import { extractSessionId, errorMessage } from '../../util/utilFunctions.js';
+import jsonParserMiddleware from '../../middlewares/jsonParser.js';
+import hashPasswordMiddleware from '../../middlewares/hashPassword.js';
+import authenticateMiddleware from '../../middlewares/authenticate.js';
 
 export default async function handler(req, res) {
   jsonParserMiddleware(req, res);
-  hashPasswordMiddleware(req, res);
-  authenticateMiddleware(req, res);
-  const { artistName } = req.body;
-  const sessionID = extractSessionId(req);
-  if (sessionID !== null) {
-    try {
-      const inserted = await insertArtist(sessionID, artistName);
-      if (inserted) {
-        console.log("Success inserting artist");
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end("Artist inserted successfully");
+  await hashPasswordMiddleware(req, res, async () => {
+    await authenticateMiddleware(req, res, async () => {
+      const { artistName } = req.body;
+      const sessionID = extractSessionId(req);
+      if (sessionID !== null) {
+        try {
+          const inserted = await insertArtist(sessionID, artistName);
+          if (inserted) {
+            console.log('Success inserting artist');
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Artist inserted successfully');
+          } else {
+            errorMessage(res, 'Could not add artistName', 'Error');
+          }
+        } catch (error) {
+          errorMessage(res, error, 'Error adding artistName');
+        }
       } else {
-        errorMessage(res, "Could not add artistName", "Error");
+        errorMessage(res, 'Unable to extract sessionID', 'Error');
       }
-    } catch (error) {
-      errorMessage(res, error, "Error adding artistName");
-    }
-  } else {
-    errorMessage(res, "Unable to extract sessionID", "Error");
-  }
+    });
+  });
 }
--- coogify-app/backend/middlewares/authenticate.js
+++ coogify-app/backend/middlewares/authenticate.js
@@ -1,17 +1,18 @@
-import { sessionExists } from "../Session/sessionManager.js";
+// authenticateMiddleware.js
+import { sessionExists } from '../Session/sessionManager.js';
 
-export default async function authenticateMiddleware(request, response) {
-  console.log("authenticating");
+export default async function authenticateMiddleware(request, response, next) {
+  console.log('authenticating');
   const path = new URL(request.url, `http://${process.env.MYSQL_HOST}`)
     .pathname;
   console.log(path);
 
   // Check if the request path is not login or register
-  if (path !== "/api/login" && path !== "/api/register") {
-    console.log("not in login or register");
+  if (path !== '/api/login' && path !== '/api/register') {
+    console.log('not in login or register');
 
     // Check if the Authorization header is present
-    const authHeader = request.headers["authorization"];
+    const authHeader = request.headers['authorization'];
     console.log(authHeader);
     if (!authHeader) {
       unauthorized(response);
@@ -19,7 +20,7 @@ export default async function authenticateMiddleware(request, response) {
     }
 
     // Extract the sessionID from the Authorization header
-    const [, sessionID] = authHeader.split(" ");
+    const [, sessionID] = authHeader.split(' ');
 
     // Check if the session is valid
     try {
@@ -33,8 +34,9 @@ export default async function authenticateMiddleware(request, response) {
       return;
     }
   }
+  next(); // Proceed to the next middleware or route handler
 }
 
 function unauthorized(response) {
-  response.status(401).send("Unauthorized");
+  response.status(401).send('Unauthorized');
 }
