c611b967bd006e5fbea361b8ae673099dbb1a96d
Jacob Rangel <jacobrangel0628@gmail.com>
2024-04-08 00:58:03 -0500

    changed port 3000 back to 3001

--- coogify-app/frontend/src/apiConfig.ts
+++ coogify-app/frontend/src/apiConfig.ts
@@ -1,4 +1,4 @@
 // src/apiConfig.ts
-const backendBaseUrl = 'http://localhost:3000'; // Update with your actual backend URL
+const backendBaseUrl = 'http://localhost:3001'; // Update with your actual backend URL
 
 export default backendBaseUrl;
