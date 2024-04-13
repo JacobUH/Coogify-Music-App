3d7b24c2d354bb0c1ddee96bb341b71192dc41ee
Shruthi Yenamagandla <yshruthi.14@gmail.com>
2024-04-13 15:58:00 -0500

    Fixed the extendedSongs issue happening in music rows & filled track table fully

--- coogify-app/frontend/src/components/interface/extendedPages/extendedClassicalSongs.tsx
+++ coogify-app/frontend/src/components/interface/extendedPages/extendedClassicalSongs.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import backendBaseUrl from '../../../apiConfig';
-import { Link } from 'react-router-dom';
 
 interface Song {
   trackID: number;
@@ -42,8 +41,8 @@ export const ExtendedClassicalSongs = ({ title }: Props) => {
 
   const storedToken = localStorage.getItem('sessionToken');
 
+  // FETCH TOP SONGS BACKEND CALL
   useEffect(() => {
-    // Fetch data from backend API for rap songs
     const fetchClassicalSongs = async () => {
       try {
         const response = await axios.get(
@@ -55,10 +54,10 @@ export const ExtendedClassicalSongs = ({ title }: Props) => {
             },
           }
         );
-
+        //console.log(response.data);
         setClassicalSongs(response.data);
       } catch (error) {
-        console.error('Error fetching Classical songs:', error);
+        console.error('Error fetching new songs:', error);
       }
     };
 
@@ -102,28 +101,26 @@ export const ExtendedClassicalSongs = ({ title }: Props) => {
 
   return (
     <div className="w-full flex flex-col md:gap-4 gap-6 px-2">
-      <div className="w-full flex items-center justify-between">
-        <span className="text-[22px]">{title}</span>
-        <Link to="/ClassicalSongs" className="text-[#9E67E4] text-[15px] font-medium">
-          See More
-        </Link>
-      </div>
       <div className="w-full flex items-center overflow-x-auto overflow-y-auto md:pb-0 pb-5">
-        <div className="flex items-center gap-2">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6">
           {ClassicalSongs.map((song: Song) => {
             return (
               <div
                 key={song.songName}
                 className="flex flex-col items-center gap-[6px] cursor-pointer"
-                style={{ minWidth: '200px' }} // Adjust the minimum width of each song item
+                style={{ minWidth: '200px' }}
                 onClick={(e) => handleSongClick(song, e)}
               >
-                <div className=" bg-[#656262] rounded-lg p-5 bg-center bg-cover">
+                <div className="bg-[#656262] rounded-lg p-5 bg-center bg-cover relative">
+                  {song.isPopular ? (
+                    <div className="absolute bottom-1 right-1 w-3 h-3 bg-blue-500 rounded-full"></div>
+                  ) : null}
                   <img
                     className="w-[140px] h-[140px] rounded-xl"
                     src={song.coverArtURL}
                     alt={song.songName}
                   />
+
                   <div className="pt-2 text-white text-[15px] font-medium whitespace-nowrap">
                     {song.songName.length > 20
                       ? song.songName.slice(0, 17) + '...'
