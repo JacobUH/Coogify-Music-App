8a180b80d1bd0bc903af46cc64fc032724662455
lindolfo1 <cuentas@feptec.com>
2024-04-19 21:47:38 -0500

    modified:   coogify-app/frontend/src/apiConfig.ts

--- coogify-app/frontend/src/apiConfig.ts
+++ coogify-app/frontend/src/apiConfig.ts
@@ -1,4 +1,4 @@
 // src/apiConfig.ts
-const backendBaseUrl = 'https://coogify-frontend.vercel.app'; // Update with your actual backend URL
+const backendBaseUrl = 'https://coogify-backend-five.vercel.app/'; // Update with your actual backend URL
 
 export default backendBaseUrl;
