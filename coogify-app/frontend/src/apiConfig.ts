b74389692340fc8c72ff4c385273dad1726cae91
lindolfo1 <cuentas@feptec.com>
2024-04-20 23:51:34 -0500

    modified:   coogify-app/frontend/src/apiConfig.ts

--- coogify-app/frontend/src/apiConfig.ts
+++ coogify-app/frontend/src/apiConfig.ts
@@ -1,3 +1,3 @@
-const backendBaseUrl = 'http://3.17.190.103:3001'; // Update with your actual backend URL
+const backendBaseUrl = 'http://3.18.22.13:3001'; // Update with your actual backend URL
 
 export default backendBaseUrl;
