0325f60573f6b088edf97f26f3b8fcec0b6c20a3
lindolfo1 <cuentas@feptec.com>
2024-04-22 01:15:07 -0500

    modified:   coogify-app/frontend/src/components/interface/elements/AccountDropdown.tsx 	modified:   coogify-app/frontend/src/components/interface/elements/notificationsPopup.tsx 	modified:   coogify-app/frontend/src/components/interface/mainPages/HomeMain.tsx

--- coogify-app/frontend/src/components/interface/elements/notificationsPopup.tsx
+++ coogify-app/frontend/src/components/interface/elements/notificationsPopup.tsx
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import backendBaseUrl from '../../../apiConfig';
+import React from 'react';
 import Logo from '/images/Logo.svg';
 
 interface Notification {
@@ -10,33 +8,15 @@ interface Notification {
   trackID: number | null;
 }
 
-const storedToken = localStorage.getItem('sessionToken');
+interface NotificationsProps {
+  notifications: Notification[];
+  onDismiss: () => void;
+}
 
-export const Notifications: React.FC = () => {
-  const [notifications, setNotifications] = useState<Notification[]>([]);
-
-  useEffect(() => {
-    fetchNotifications();
-  }, []);
-
-  const fetchNotifications = async () => {
-    try {
-      const response = await axios.get(`${backendBaseUrl}/api/notifications`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('sessionToken')}`,
-          'Content-Type': 'application/json',
-        },
-      });
-      setNotifications(response.data);
-    } catch (error) {
-      console.error('Error fetching notifications:', error);
-    }
-  };
-
-  const handleDismiss = () => {
-    // Close the notifications popup or handle dismiss logic here
-  };
-
+export const Notifications: React.FC<NotificationsProps> = ({
+  notifications,
+  onDismiss,
+}) => {
   return (
     <>
       <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
@@ -57,7 +37,7 @@ export const Notifications: React.FC = () => {
           </div>
           <button
             className="bg-[#656262] hover:bg-[#9E67E4] text-white font-bold mt-4 py-2 px-10 rounded"
-            onClick={handleDismiss}
+            onClick={onDismiss}
           >
             Dismiss
           </button>
